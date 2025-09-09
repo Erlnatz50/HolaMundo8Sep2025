752a8e383cd15375362f0057039d48fbf6789a9e
Erlnatz50 <erlantzgarcia05@gmail.com>
2025-09-09 09:30:56 +0200

    Añadiendo el primer ejercicio del powerpoint

--- src/main/java/es/erlantzg/App.java
+++ src/main/java/es/erlantzg/App.java
@@ -2,8 +2,10 @@ package es.erlantzg;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,20 +17,29 @@ public class App extends Application {
 
     private static Scene scene;
 
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
-
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+    */
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Stage with a Sized Scene");
+        Group root = new Group(new Button("Hello"));
+        Scene scene = new Scene(root, 300, 100);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
