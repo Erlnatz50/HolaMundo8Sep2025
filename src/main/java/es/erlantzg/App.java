8ee464ba59cea1860866e4612fb97fc6869485b2
Erlnatz50 <erlantzgarcia05@gmail.com>
2025-09-11 12:30:43 +0200

    funciona todo y añadido javadoc

--- src/main/java/es/erlantzg/App.java
+++ src/main/java/es/erlantzg/App.java
@@ -2,10 +2,7 @@ package es.erlantzg;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,29 +12,12 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
-    /*
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-    */
+        FXMLLoader loaded = new FXMLLoader(getClass().getResource("/fxml/holamundo.fxml"));
+        Scene scene = new Scene(loaded.load(), 320, 240);
 
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Stage with a Sized Scene");
-        Group root = new Group(new Button("Hello"));
-        Scene scene = new Scene(root, 300, 100);
+        stage.setTitle("Hola Mundo desde FXML");
         stage.setScene(scene);
         stage.show();
     }
